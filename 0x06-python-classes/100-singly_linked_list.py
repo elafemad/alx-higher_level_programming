b46c9f0f2e8d90a9c0adb155efa6923fd232fcd1
elafemad <elafemad02@gmail.com>
2024-01-23 18:47:44 -0800

    edit identation in 1 and 0, other tasks

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+"""Defines the classes of SinglyLinkedList"""
+
+
+class Node:
+    """
+    Class Node.
+
+    Attributes:
+        data: data of node.
+    """
+    def __init__(self, data, next_node=None):
+        """Creates constructor of node.
+
+        Args:
+            __data : data field of linklist.
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Getter method.
+
+        Returns: int value of data field.
+        """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Method of setter for data.
+
+        Args:
+            value (int): data field of a node.
+
+        Raises:
+            TypeError: data must be an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Retrives the next_node instance.
+
+        Returns: The next_node instance.
+        """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Setter for Node.
+
+        Args:
+            value (None): next node of a Node.
+
+        Raises:
+            TypeError: next_node must be a Node object .
+        """
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """
+    Class of SinglyLinkedList.
+
+    Attributes:
+        head: head of List.
+    """
+    def __init__(self):
+        """Construction of SinglyLinkedList.
+
+        Args:
+            __head : head of the SinglyLinkedList.
+        """
+        self.__head = None
+
+    def __str__(self):
+        """Represents the class objects.
+
+        Returns: The class object.
+        """
+        temp = self.__head
+        print_node = []
+        while temp is not None:
+            print_node.sort()
+            print_node.append(str(temp.data))
+            temp = temp.next_node
+
+        print_node.sort(key=int)
+        return ("\n".join(print_node))
+
+    def sorted_insert(self, value):
+        """Inserts a new node.
+
+        Args:
+            value: value.
+        """
+        if self.__head is None:
+            new_node = Node(value)
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            new_node = Node(value)
+            new_node.data = value
+            new_node.next_node = self.__head
+            self.__head = new_node
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This define square class"""
+
+
+class Square:
+    """
+    Class has no attribute and construct have none.
+    Attributes:
+       size: private instance size of the square.
+    """
+    def __init__(self, size=0):
+        """Constructor of square with one instance size.
+        Args:
+            size: private instance size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """Method cal the area of square.
+
+        Returns: the square area.
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrive size.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method.
+
+        Args:
+            value (int): nust be int value.
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """method to print # in square
+        """
+
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * (self.__size))
